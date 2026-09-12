3e67c81d64c290e46fd08452c5bb95ccc5845495
Roman-Luzyanin <luzyanin.ro@gmail.com>
2026-09-12 21:26:32 +0300

    Update events.js

--- src/UI/events.js
+++ src/UI/events.js
@@ -228,7 +228,7 @@ const initTodoEvents = () => {
 																															'No time limit';												 				 
 			completedAt.textContent = format(todo.addedTo, 'd MMMM yyyy');
 			
-			if (!todo.dueDate || isBefore(todo.addedTo, todo.dueDate)) {
+			if (isBefore(todo.addedTo, todo.dueDate)) {
 				resultImg.classList.add('onTime');
 				resultImg.classList.remove('overDue');
 				resultText.textContent = 'On Time!';
